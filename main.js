8f22c000f14c34cd60601c561b941b4a531443e8
markwst <wongsathorn_w@cmu.ac.th>
2022-07-17 15:57:33 +0700

    5.6

--- main.js
+++ main.js
@@ -1,23 +1,47 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Shoes',
-            brand: 'SE 331',
-            //image: './assets/images/socks_green.jpg',
-            //inStock: false,
-            inventory: 100,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 5 }
-            ],
-            selectedVariant: 0,
+            // product: 'Shoes',
+
+            // brand: 'SE 331',
+            // // image: './assets/images/socks_green.jpg',
+            // // inStock: false,
+            // inventory: 100,
+            // details: ['50% cotton', '30% wool', '20% polyester'],
+            // variants: [
+            //     { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+            //     { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
+            // ],
+            // selectedVariant:0,
             cart: [0, 0],
-            onSale: true,
-            premuim: true
+            // onSale: true
+            premium: true
 
         }
     },
+    // methods: {
+    //     addToCart() {
+    //         this.cart += 1
+    //     },
+    //     updateImage(variantImage) {
+    //         this.image = variantImage
+    //     },
+    //     updateVariant(index){
+    //         this.selectedVariant = index;
+    //     }
+    // },
+    // computed:{
+    //     title(){
+    //         return this.brand + ' ' + this.product +(this.onSale ? " On Sale" : "")
+    //     },
+    //     image(){
+    //         return this.variants[this.selectedVariant].image
+    //     },
+    //     inStock(){
+    //         return this.variants[this.selectedVariant].quantity
+    //     },
+
+    // }
     methods: {
         updateCart(id) {
             this.cart.push(id);
@@ -26,33 +50,10 @@ const app = Vue.createApp({
             } else
                 this.cart[1] += 1;
         },
-        addToCart() {
-            this.cart += 1
-        },
-        updateImage(variantImage) {
-            this.image = variantImage
-        },
-        updateVariant(index) {
-            this.selectedVariant = index;
+        removeFromCart() {
+            this.cart.splice(0, 1, 0)
+            this.cart.splice(1, 1, 0)
         }
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product + (this.onSale ? " On Sale" : "")
-        },
-        image() {
-            return this.variants[this.selectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
-        },
-        shipping() {
-            if (this.premuim) {
-                return 'Free'
-            }
-            return 30
-        }
-
     }
 
 })
